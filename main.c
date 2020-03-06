0e2712a5b094b82fda333534692a116dcfcd0acb
Nathan Lafarge <lafarge.nathan3b3@gmail.com>
2020-03-06 19:31:57 +0100

    update

--- main.c
+++ main.c
@@ -12,40 +12,7 @@
 
 #include "includes/main_include.h"
 
-int multiplication(int nombreNombresAMultiplier, ...)
-{
-    int i;
-    va_list ap;
-    int resultat;
-    int argument;
-    
-    i = 0;
-    resultat = 1;
-    va_start(ap, nombreNombresAMultiplier);
-    while (i < nombreNombresAMultiplier)
-    {
-        argument = va_arg(ap, int);
-        resultat = resultat * argument;
-        printf("Arg : %d\n", argument);
-        printf("Résultat : %d\n", resultat);
-        i++;
-    }
-    va_end(ap);
-    return(resultat);
-}
-
 int main()
 {
-    // if (ac > 1)
-    // {
-    //     ft_printf("%s", av[1])
-    //     printf("%s", av[1]);
-    // }
-
-    int resultat;
-
-    resultat = multiplication(3, 3, 5, 45);
-    printf("%d", resultat);
     
-    return (0);
 }
